0d8829bb64c9b8a115442d76be189702170c7429
Manish Singh (DHL eCom AM), external <manish.singh@dhl.com>
2026-04-03 13:58:31 +0530

    AWS Secrets Manager Integration code

--- src/main/java/com/bankingapplication/CustomerManagementService/secretsmanager/DbSecret.java
+++ src/main/java/com/bankingapplication/CustomerManagementService/secretsmanager/DbSecret.java
@@ -1,12 +1,15 @@
 package com.bankingapplication.CustomerManagementService.secretsmanager;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DbSecret {
     private String username;
     private String password;
     private String host;
     private Integer port;
     private String dbname;
+    private String engine;
 }
